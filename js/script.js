5f8c1f396e337be1dd43873a6afd04d6c6adf149
RoninsWork <130744166+verygood-agency@users.noreply.github.com>
2023-10-04 15:24:48 +0300

    доработал скрипт вызова окна для сенсорных + поправил ошибки в разметке окон

--- js/script.js
+++ js/script.js
@@ -1,26 +1,37 @@
+const handleClickOrTouch = (element, callback) => {
+    const touchendListener = (e) => {
+        e.preventDefault();
+        element.removeEventListener('touchend', touchendListener);
+        callback(e);
+    };
+    element.addEventListener('click', callback);
+    element.addEventListener('touchstart', (e) => {
+        e.preventDefault();
+        element.addEventListener('touchend', touchendListener);
+    });
+};
+
 document.addEventListener('DOMContentLoaded', function () {
-  let showDialogButtons = document.querySelectorAll('.show-dialog');
-  let targetClass = null;
+    let showDialogButtons = document.querySelectorAll('.show-dialog');
+    let targetClass = null;
 
-  const setupBodyClickListener = () => {
-      document.addEventListener('click', function listener(event) {
-          if (!event.target.closest('.dialog-windows__item') || event.target.closest('.dialog-windows__close')) {
-              document.body.classList.remove(targetClass);
-              targetClass = null;
-              // Удаляем обработчик, чтобы избежать повторного удаления класса в будущем
-              document.removeEventListener('click', listener);
-          }
-      });
-  };
+    const setupBodyClickListener = () => {
+        document.addEventListener('click', function listener(event) {
+            if (!event.target.closest('.dialog-windows__item') || event.target.closest('.dialog-windows__close')) {
+                document.body.classList.remove(targetClass);
+                targetClass = null;
+                document.removeEventListener('click', listener);
+            }
+        });
+    };
 
-  showDialogButtons.forEach(button => {
-      button.addEventListener('click', function (event) {
-          event.preventDefault();
-          // Предотвращаем всплытие события
-          event.stopPropagation();
-          targetClass = event.currentTarget.getAttribute('data-target');
-          document.body.classList.add(targetClass);
-          setupBodyClickListener();
-      });
-  });
+    showDialogButtons.forEach(button => {
+        handleClickOrTouch(button, function (event) {
+            event.preventDefault();
+            event.stopPropagation();
+            targetClass = event.currentTarget.getAttribute('data-target');
+            document.body.classList.add(targetClass);
+            setupBodyClickListener();
+        });
+    });
 });
